8af36c03f608dd7d65eec4b5cbf6599210afb6aa
MrHrat <hrat1995@gmail.com.com>
2018-11-16 03:23:07 +0200

    Adding a class to a class key relationship

---
+++ Inwardrobe/Class/ComboBoxPairs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inwardrobe.Class
+{
+    public class ComboBoxPairs
+    {
+        public string Key { get; set; }
+        public string Value
+        {
+            get
+            {
+                return Builder.GetPropertyValue(Obj, Key).ToString();
+            }
+            set
+            {
+                Builder.SetPropertyValue(Obj, Key, Convert.ToDouble(value));
+            }
+        }
+        public object Obj { get; set; }
+
+        public ComboBoxPairs(string key, object obj)
+        {
+            Key = key;
+            Obj = obj;
+        }
+    }
+}
